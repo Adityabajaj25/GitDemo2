dd4c117cda05894892ce647af5cffc5bcc763b01
Aditya Bajaj <103935420+Adityabajaj25@users.noreply.github.com>
2023-08-14 12:38:49 +0530

    Update Testclass2.java

--- src/test/java/Testclass2.java
+++ src/test/java/Testclass2.java
@@ -4,7 +4,7 @@ public class Testclass2 {
     @Test
     public void Testcase1(){
         System.out.println("test case 1 from Testclass2");
-        System.out.println("another comment");
+        System.out.println("another comment----------");
         System.out.println("another comment added in Github");
 
     }
