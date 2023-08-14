86c4bb34afe541849e99fe7f3c4234dcbe46623a
Aditya Bajaj <robinsingh21@aol.com>
2023-08-14 12:29:56 +0530

    updated testcase2 class file

--- src/test/java/Testclass2.java
+++ src/test/java/Testclass2.java
@@ -4,5 +4,7 @@ public class Testclass2 {
     @Test
     public void Testcase1(){
         System.out.println("test case 1 from Testclass2");
+        System.out.println("another comment");
+
     }
 }
